4a56db60892d0821e43b323e31c16879eab49d6e
AZIT-TRAVEL <teaseong5@gmail.com>
2025-12-15 18:42:24 +0700

    fix: add to import weasyprint

--- fulfillment/views.py
+++ fulfillment/views.py
@@ -9,6 +9,7 @@ from decimal import Decimal
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+import weasyprint
 
 # ---------------------------------------------------------
 # [1] 모델 (Models)
